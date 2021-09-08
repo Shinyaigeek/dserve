41ee667c06d854175ae14701d3ba8126b1d06517
shinyaigeek <philispaxil@gmail.com>
2021-09-08 19:09:02 +0900

    Feature: add module dirHandler

---
+++ src/dirHandler.ts
@@ -0,0 +1,23 @@
+export const dirHandler: (dir: string) => Promise<string> = async function (
+  dir
+) {
+  const directories = Deno.readDir(dir);
+
+  const templates: string[] = [];
+
+  for await (const entry of directories) {
+    const template = (() => {
+      if (entry.isFile) {
+        return `<li><a href="${dir}/${entry.name}">${entry.name}</a></li>`;
+      } else if (entry.isDirectory) {
+        return `<li><a href="${dir}/${entry.name}">${entry.name}</a></li>`;
+      } else {
+        return `<li><a href="${dir}/${entry.name}">${entry.name}</a></li>`;
+      }
+    })();
+
+    templates.push(template);
+  }
+
+  return `<ul>${templates.join("")}</ul>`;
+};
